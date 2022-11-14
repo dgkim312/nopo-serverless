cb92eac1d7ac9feedd44f892d958842df2fecc7f
Jin, Song <sonjin@amazon.com>
2022-11-14 21:22:40 +0900

    Update Lambda Function to insert to aggregates_table

--- lambda/lambda-handler.py
+++ lambda/lambda-handler.py
@@ -1,7 +1,25 @@
+import os
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+TABLE_NAME = os.environ['TABLE_NAME']
+
 def handler(event, context):
     response = event
     print(response)
+
+    table = dynamodb.Table(TABLE_NAME)
+
+    for record in event['Records']:
+        response = table.put_item(
+            Item={
+                'pk': str(record['dynamodb']['Keys']['pk']['S'])
+            }
+        )
+
     return {
         'statusCode': 200,
         'body': response
     }
+
