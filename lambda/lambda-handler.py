125690da4ee523eb8213099c14bc85719e016634
Jin, Song <sonjin@amazon.com>
2022-11-16 13:11:30 +0900

    Add all attributes to target DDB table

--- lambda/lambda-handler.py
+++ lambda/lambda-handler.py
@@ -13,17 +13,18 @@ def handler(event, context):
     table = dynamodb.Table(TABLE_NAME)
 
     for record in event['Records']:
-        new_image = record['dynamodb']['NewImage']
 
-        item = {'pk': str(record['dynamodb']['Keys']['pk']['S']),
-                'RATING_AVG': str(round(int(new_image['RATING_SUM']['N'])
-                                        / int(new_image['RATING_COUNT']['N']), 2))
-                }
+        new_image = record['dynamodb']['NewImage'] | record['dynamodb']['Keys']
+
+        item = {
+            'RATING_AVG': str(round(int(new_image['RATING_SUM']['N'])
+                                    / int(new_image['RATING_COUNT']['N']), 2))
+        }
+
         for key, value in new_image.items():
             item[key] = [int(v) if k == 'N' else v for k, v in value.items()][0]
-        response = table.put_item(
-            Item=item
-        )
+
+        response = table.put_item(Item=item)
 
     return {
         'statusCode': 200,
