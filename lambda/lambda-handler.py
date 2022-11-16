370976c48f11c26fda68231db65fd68658f43c47
Jin, Song <sonjin@amazon.com>
2022-11-16 13:04:52 +0900

    Add all attributes to target DDB table

--- lambda/lambda-handler.py
+++ lambda/lambda-handler.py
@@ -5,6 +5,7 @@ dynamodb = boto3.resource('dynamodb')
 
 TABLE_NAME = os.environ['TABLE_NAME']
 
+
 def handler(event, context):
     response = event
     print(response)
@@ -12,14 +13,19 @@ def handler(event, context):
     table = dynamodb.Table(TABLE_NAME)
 
     for record in event['Records']:
+        new_image = record['dynamodb']['NewImage']
+
+        item = {'pk': str(record['dynamodb']['Keys']['pk']['S']),
+                'RATING_AVG': str(round(int(new_image['RATING_SUM']['N'])
+                                        / int(new_image['RATING_COUNT']['N']), 2))
+                }
+        for key, value in new_image.items():
+            item[key] = [int(v) if k == 'N' else v for k, v in value.items()][0]
         response = table.put_item(
-            Item={
-                'pk': str(record['dynamodb']['Keys']['pk']['S'])
-            }
+            Item=item
         )
 
     return {
         'statusCode': 200,
         'body': response
     }
-
